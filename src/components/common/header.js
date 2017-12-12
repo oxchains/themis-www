9bb2be35228998d180b07ded492d6c6ac080560f
liuruichao <liuruichao555@126.com>
2017-12-12 18:03:53 +0800

    update circuit backbroundimg

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -6,7 +6,7 @@ import {ROOT_URL} from '../../actions/types';
 class Header extends Component{
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {};
     }
 
     componentDidMount() {
